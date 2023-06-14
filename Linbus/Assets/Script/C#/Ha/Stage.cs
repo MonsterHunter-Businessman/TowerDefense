99c7a5761361cb34268aa43c715ce20351802175
aprnald74 <a010313813321@gmail.com>
2023-06-14 19:37:34 +0900

    :white_check_mark: 몬스터 이동 구현 / 스테이지 마다 다르게 이동 구현

--- Linbus/Assets/Script/C#/Ha/Stage.cs
+++ Linbus/Assets/Script/C#/Ha/Stage.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    public GameObject ehsRktm;
+    public GameObject goblin;
 
     public Transform mS;
 
-    public int[] stage = new int[] {5, 7, 9, 10};
+    public int stage;
 
-    Vector3[] stO = { new Vector3 { x = 7, y = 0, z = 0 }, new Vector3 { x = -7, y = 0, z = 0 } };
+    public GameObject monsterB;
+
+    public GameObject parent;
+
+    public int[] stageMonster = new int[] {5, 7, 9, 10};
+
+    Vector3[] st1 = { new Vector3 { x = 7, y = 0, z = 0 }, new Vector3 { x = 0, y = 0, z = 0}, new Vector3 { x = -7, y = 0, z = 0 } };
+
+    Vector3[] st2 = { new Vector3 { x = 9, y = 0, z = 0 }, new Vector3 { x = 3, y = 0, z = 0 }, new Vector3 { x = 3, y = -1, z = 0 }, new Vector3 { x = 1, y = -1, z = 0 }, new Vector3 { x = 1, y = 0, z = 0 }};
+
 
     void Start()
     {
@@ -21,14 +30,23 @@ public class Stage : MonoBehaviour
     void monsterSp()
     {
 
-        Instantiate(ehsRktm, mS.transform);
+        if (stage == 1) {
 
+            monsterB = Instantiate(goblin, mS.transform);
 
+            monsterB.transform.parent = parent.transform;
+
+            monsterB.GetComponent<MonsterGoblin>().pathval = new Vector3[2];
+            monsterB.GetComponent<MonsterGoblin>().pathval = st1;
+
+        }
+
+        
 
 
     }
 
-
+    //GameObject.Find("스크립트를 포함하는 오브젝트이름").GetComponent<스크립트 이름>().변수
 
 
     void Update()
